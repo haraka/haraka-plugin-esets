6b479734c9fa6f8acbc291018ae003c2045f73a9
Matt Simerson <matt@tnpi.net>
2026-05-28 23:02:47 -0700

    test: refactored against test-fixtures 1.7.0 (#2)

--- test/index.js
+++ test/index.js
@@ -2,13 +2,13 @@ const assert = require('node:assert/strict')
 const { beforeEach, describe, it } = require('node:test')
 
 // npm modules
-const fixtures = require('haraka-test-fixtures')
+const { makeConnection, makePlugin } = require('haraka-test-fixtures')
 
 // start of tests
 //    assert: https://nodejs.org/api/assert.html
 
 beforeEach(function () {
-  this.plugin = new fixtures.plugin('esets')
+  this.plugin = makePlugin('esets', { register: false })
 })
 
 describe('esets', function () {
@@ -26,13 +26,12 @@ describe('load_esets_ini', function () {
 
 describe('uses text fixtures', function () {
   it('sets up a connection', function () {
-    this.connection = fixtures.connection.createConnection({})
+    this.connection = makeConnection()
     assert.ok(this.connection.server)
   })
 
   it('sets up a transaction', function () {
-    this.connection = fixtures.connection.createConnection({})
-    this.connection.transaction = fixtures.transaction.createTransaction({})
+    this.connection = makeConnection({ withTxn: true })
     assert.ok(this.connection.transaction.header)
   })
 })
